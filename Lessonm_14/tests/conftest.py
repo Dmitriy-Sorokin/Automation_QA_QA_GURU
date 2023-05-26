4ddffb9c1d1ca43af925a7e8a9ff53286f48e44f
Dmitriy Sorokin <sorokin.dm@promail366.com>
2023-05-26 19:29:13 +0300

    api

---
+++ Lessonm_14/tests/conftest.py
@@ -0,0 +1,30 @@
+import os
+
+import pytest
+from dotenv import load_dotenv
+
+from Lessonm_14.utils.base_session import BaseSession
+
+
+load_dotenv()
+
+
+def pytest_addoption(parser):
+    parser.addoption("--env")
+
+
+@pytest.fixture(scope="session")
+def env(request):
+    return request.config.getoption("--env")
+
+
+@pytest.fixture(scope="session")
+def demoshop():
+    api_url = os.getenv("API_URL")
+    return BaseSession(api_url)
+
+
+@pytest.fixture(scope="session")
+def reqres():
+    api_url = "https://reqres.in"
+    return BaseSession(api_url)
